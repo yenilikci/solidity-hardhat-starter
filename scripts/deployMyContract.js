5e374e070850b98c2415f0c84784ae0c7e0be071
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-03-10 02:28:29 +0300

    files added

---
+++ scripts/deployMyContract.js
@@ -0,0 +1,14 @@
+async function main() {
+    const MyContract = await ethers.getContractFactory("MyContract");
+    const myContract = await MyContract.deploy("My Contract");
+  
+    console.log("My Contract deployed to:", myContract.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+  });
+  
